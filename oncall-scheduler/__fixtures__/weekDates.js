86e1579deb8f9063dca35c5b8b2d4e3cf4feadd1
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-01-14 11:51:14 +0000

    add fixtures for weekDates

---
+++ oncall-scheduler/__fixtures__/weekDates.js
@@ -0,0 +1,447 @@
+const weekdaysFor2022 = [
+  [
+      new Date(2022, 0, 3),
+      new Date(2022, 0, 4),
+      new Date(2022, 0, 5),
+      new Date(2022, 0, 6),
+      new Date(2022, 0, 7)
+  ],
+  [
+      new Date(2022, 0, 10),
+      new Date(2022, 0, 11),
+      new Date(2022, 0, 12),
+      new Date(2022, 0, 13),
+      new Date(2022, 0, 14)
+  ],
+  [
+      new Date(2022, 0, 17),
+      new Date(2022, 0, 18),
+      new Date(2022, 0, 19),
+      new Date(2022, 0, 20),
+      new Date(2022, 0, 21)
+  ],
+  [
+      new Date(2022, 0, 24),
+      new Date(2022, 0, 25),
+      new Date(2022, 0, 26),
+      new Date(2022, 0, 27),
+      new Date(2022, 0, 28),
+  ],
+  [
+      new Date(2022, 0, 31),
+      new Date(2022, 1, 1),
+      new Date(2022, 1, 2),
+      new Date(2022, 1, 3),
+      new Date(2022, 1, 4),
+  ],
+  [
+      new Date(2022, 1, 7),
+      new Date(2022, 1, 8),
+      new Date(2022, 1, 9),
+      new Date(2022, 1, 10),
+      new Date(2022, 1, 11),
+  ],
+  [
+      new Date(2022, 1, 14),
+      new Date(2022, 1, 15),
+      new Date(2022, 1, 16),
+      new Date(2022, 1, 17),
+      new Date(2022, 1, 18),
+  ],
+  [
+      new Date(2022, 1, 21),
+      new Date(2022, 1, 22),
+      new Date(2022, 1, 23),
+      new Date(2022, 1, 24),
+      new Date(2022, 1, 25),
+  ],
+  [
+      new Date(2022, 1, 28),
+      new Date(2022, 2, 1),
+      new Date(2022, 2, 2),
+      new Date(2022, 2, 3),
+      new Date(2022, 2, 4),
+  ],
+  [
+      new Date(2022, 2, 7),
+      new Date(2022, 2, 8),
+      new Date(2022, 2, 9),
+      new Date(2022, 2, 10),
+      new Date(2022, 2, 11),
+  ],
+  [
+      new Date(2022, 2, 14),
+      new Date(2022, 2, 15),
+      new Date(2022, 2, 16),
+      new Date(2022, 2, 17),
+      new Date(2022, 2, 18),
+  ],
+  [
+      new Date(2022, 2, 21),
+      new Date(2022, 2, 22),
+      new Date(2022, 2, 23),
+      new Date(2022, 2, 24),
+      new Date(2022, 2, 25),
+  ],
+  [
+      new Date(2022, 2, 28),
+      new Date(2022, 2, 29),
+      new Date(2022, 2, 30),
+      new Date(2022, 2, 31),
+      new Date(2022, 3, 1),
+  ],
+  [
+      new Date(2022, 3, 4),
+      new Date(2022, 3, 5),
+      new Date(2022, 3, 6),
+      new Date(2022, 3, 7),
+      new Date(2022, 3, 8),
+  ],
+  [
+      new Date(2022, 3, 11),
+      new Date(2022, 3, 12),
+      new Date(2022, 3, 13),
+      new Date(2022, 3, 14),
+      new Date(2022, 3, 15),
+  ],
+  [
+      new Date(2022, 3, 18),
+      new Date(2022, 3, 19),
+      new Date(2022, 3, 20),
+      new Date(2022, 3, 21),
+      new Date(2022, 3, 22),
+  ],
+  [
+      new Date(2022, 3, 25),
+      new Date(2022, 3, 26),
+      new Date(2022, 3, 27),
+      new Date(2022, 3, 28),
+      new Date(2022, 3, 29),
+  ],
+  [
+      new Date(2022, 4, 2),
+      new Date(2022, 4, 3),
+      new Date(2022, 4, 4),
+      new Date(2022, 4, 5),
+      new Date(2022, 4, 6),
+  ],
+  [
+      new Date(2022, 4, 9),
+      new Date(2022, 4, 10),
+      new Date(2022, 4, 11),
+      new Date(2022, 4, 12),
+      new Date(2022, 4, 13),
+  ],
+  [
+      new Date(2022, 4, 16),
+      new Date(2022, 4, 17),
+      new Date(2022, 4, 18),
+      new Date(2022, 4, 19),
+      new Date(2022, 4, 20),
+  ],
+  [
+      new Date(2022, 4, 23),
+      new Date(2022, 4, 24),
+      new Date(2022, 4, 25),
+      new Date(2022, 4, 26),
+      new Date(2022, 4, 27),
+  ],
+  [
+      new Date(2022, 4, 30),
+      new Date(2022, 4, 31),
+      new Date(2022, 5, 1),
+      new Date(2022, 5, 2),
+      new Date(2022, 5, 3),
+  ],
+  [
+      new Date(2022, 5, 6),
+      new Date(2022, 5, 7),
+      new Date(2022, 5, 8),
+      new Date(2022, 5, 9),
+      new Date(2022, 5, 10),
+  ],
+  [
+      new Date(2022, 5, 13),
+      new Date(2022, 5, 14),
+      new Date(2022, 5, 15),
+      new Date(2022, 5, 16),
+      new Date(2022, 5, 17),
+  ],
+  [
+      new Date(2022, 5, 20),
+      new Date(2022, 5, 21),
+      new Date(2022, 5, 22),
+      new Date(2022, 5, 23),
+      new Date(2022, 5, 24),
+  ],
+  [
+      new Date(2022, 5, 27),
+      new Date(2022, 5, 28),
+      new Date(2022, 5, 29),
+      new Date(2022, 5, 30),
+      new Date(2022, 6, 1),
+  ],
+  [
+      new Date(2022, 6, 4),
+      new Date(2022, 6, 5),
+      new Date(2022, 6, 6),
+      new Date(2022, 6, 7),
+      new Date(2022, 6, 8),
+  ],
+  [
+      new Date(2022, 6, 11),
+      new Date(2022, 6, 12),
+      new Date(2022, 6, 13),
+      new Date(2022, 6, 14),
+      new Date(2022, 6, 15),
+  ],
+  [
+      new Date(2022, 6, 18),
+      new Date(2022, 6, 19),
+      new Date(2022, 6, 20),
+      new Date(2022, 6, 21),
+      new Date(2022, 6, 22),
+  ],
+  [
+      new Date(2022, 6, 25),
+      new Date(2022, 6, 26),
+      new Date(2022, 6, 27),
+      new Date(2022, 6, 28),
+      new Date(2022, 6, 29),
+  ],
+  [
+      new Date(2022, 7, 1),
+      new Date(2022, 7, 2),
+      new Date(2022, 7, 3),
+      new Date(2022, 7, 4),
+      new Date(2022, 7, 5),
+  ],
+  [
+      new Date(2022, 7, 8),
+      new Date(2022, 7, 9),
+      new Date(2022, 7, 10),
+      new Date(2022, 7, 11),
+      new Date(2022, 7, 12),
+  ],
+  [
+      new Date(2022, 7, 15),
+      new Date(2022, 7, 16),
+      new Date(2022, 7, 17),
+      new Date(2022, 7, 18),
+      new Date(2022, 7, 19),
+  ],
+  [
+      new Date(2022, 7, 22),
+      new Date(2022, 7, 23),
+      new Date(2022, 7, 24),
+      new Date(2022, 7, 25),
+      new Date(2022, 7, 26),
+  ],
+  [
+      new Date(2022, 7, 29),
+      new Date(2022, 7, 30),
+      new Date(2022, 7, 31),
+      new Date(2022, 8, 1),
+      new Date(2022, 8, 2),
+  ],
+  [
+      new Date(2022, 8, 5),
+      new Date(2022, 8, 6),
+      new Date(2022, 8, 7),
+      new Date(2022, 8, 8),
+      new Date(2022, 8, 9),
+  ],
+  [
+      new Date(2022, 8, 12),
+      new Date(2022, 8, 13),
+      new Date(2022, 8, 14),
+      new Date(2022, 8, 15),
+      new Date(2022, 8, 16),
+  ],
+  [
+      new Date(2022, 8, 19),
+      new Date(2022, 8, 20),
+      new Date(2022, 8, 21),
+      new Date(2022, 8, 22),
+      new Date(2022, 8, 23),
+  ],
+  [
+      new Date(2022, 8, 26),
+      new Date(2022, 8, 27),
+      new Date(2022, 8, 28),
+      new Date(2022, 8, 29),
+      new Date(2022, 8, 30),
+  ],
+  [
+      new Date(2022, 9, 3),
+      new Date(2022, 9, 4),
+      new Date(2022, 9, 5),
+      new Date(2022, 9, 6),
+      new Date(2022, 9, 7),
+  ],
+  [
+      new Date(2022, 9, 10),
+      new Date(2022, 9, 11),
+      new Date(2022, 9, 12),
+      new Date(2022, 9, 13),
+      new Date(2022, 9, 14),
+  ],
+  [
+      new Date(2022, 9, 17),
+      new Date(2022, 9, 18),
+      new Date(2022, 9, 19),
+      new Date(2022, 9, 20),
+      new Date(2022, 9, 21),
+  ],
+  [
+      new Date(2022, 9, 24),
+      new Date(2022, 9, 25),
+      new Date(2022, 9, 26),
+      new Date(2022, 9, 27),
+      new Date(2022, 9, 28),
+  ],
+  [
+      new Date(2022, 9, 31),
+      new Date(2022, 10, 1),
+      new Date(2022, 10, 2),
+      new Date(2022, 10, 3),
+      new Date(2022, 10, 4),
+  ],
+  [
+      new Date(2022, 10, 7),
+      new Date(2022, 10, 8),
+      new Date(2022, 10, 9),
+      new Date(2022, 10, 10),
+      new Date(2022, 10, 11),
+  ],
+  [
+      new Date(2022, 10, 14),
+      new Date(2022, 10, 15),
+      new Date(2022, 10, 16),
+      new Date(2022, 10, 17),
+      new Date(2022, 10, 18),
+  ],
+  [
+      new Date(2022, 10, 21),
+      new Date(2022, 10, 22),
+      new Date(2022, 10, 23),
+      new Date(2022, 10, 24),
+      new Date(2022, 10, 25),
+  ],
+  [
+      new Date(2022, 10, 28),
+      new Date(2022, 10, 29),
+      new Date(2022, 10, 30),
+      new Date(2022, 11, 1),
+      new Date(2022, 11, 2),
+  ],
+  [
+      new Date(2022, 11, 5),
+      new Date(2022, 11, 6),
+      new Date(2022, 11, 7),
+      new Date(2022, 11, 8),
+      new Date(2022, 11, 9),
+  ],
+  [
+      new Date(2022, 11, 12),
+      new Date(2022, 11, 13),
+      new Date(2022, 11, 14),
+      new Date(2022, 11, 15),
+      new Date(2022, 11, 16),
+  ],
+  [
+      new Date(2022, 11, 19),
+      new Date(2022, 11, 20),
+      new Date(2022, 11, 21),
+      new Date(2022, 11, 22),
+      new Date(2022, 11, 23),
+  ],
+  [
+      new Date(2022, 11, 26),
+      new Date(2022, 11, 27),
+      new Date(2022, 11, 28),
+      new Date(2022, 11, 29),
+      new Date(2022, 11, 30),
+  ]
+]
+
+const weekdaysForOneWeek = [
+    [
+        new Date(2022, 0, 3),
+        new Date(2022, 0, 4),
+        new Date(2022, 0, 5),
+        new Date(2022, 0, 6),
+        new Date(2022, 0, 7)
+    ]
+]
+
+const weekdaysFromStartToMidForOneWeek = [
+    [
+        new Date(2022, 0, 10),
+        new Date(2022, 0, 11),
+        new Date(2022, 0, 12)
+    ]
+]
+
+const weekdaysFromMidToEndForOneWeek = [
+    [
+        new Date(2022, 0, 12),
+        new Date(2022, 0, 13),
+        new Date(2022, 0, 14)
+    ]
+]
+
+const weekdaysForTwoWeeks = [
+    [
+        new Date(2022, 0, 3),
+        new Date(2022, 0, 4),
+        new Date(2022, 0, 5),
+        new Date(2022, 0, 6),
+        new Date(2022, 0, 7)
+    ],
+    [
+        new Date(2022, 0, 10),
+        new Date(2022, 0, 11),
+        new Date(2022, 0, 12),
+        new Date(2022, 0, 13),
+        new Date(2022, 0, 14)
+    ]
+]
+
+const weekdaysFromStartToMidForTwoWeeks = [
+    ...weekdaysForOneWeek,
+    ...weekdaysFromStartToMidForOneWeek
+]
+
+const weekdaysFromMidToEndForTwoWeeks = [
+    [
+        new Date(2022, 0, 5),
+        new Date(2022, 0, 6),
+        new Date(2022, 0, 7)
+    ],
+    weekdaysForTwoWeeks[1]
+]
+
+const weekdaysFromMidToMidForTwoWeeks = [
+    [
+        new Date(2022, 0, 5),
+        new Date(2022, 0, 6),
+        new Date(2022, 0, 7),
+    ],
+    [
+        new Date(2022, 0, 10),
+        new Date(2022, 0, 11),
+        new Date(2022, 0, 12)
+    ]
+]
+
+module.exports = {
+  weekdaysFor2022,
+  weekdaysForOneWeek,
+  weekdaysFromStartToMidForOneWeek,
+  weekdaysFromMidToEndForOneWeek,
+  weekdaysFromStartToMidForTwoWeeks,
+  weekdaysFromMidToEndForTwoWeeks,
+  weekdaysFromMidToMidForTwoWeeks,
+  weekdaysForTwoWeeks
+}
