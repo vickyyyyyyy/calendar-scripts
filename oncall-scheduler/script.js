1185f4c2196ed128a2921341bd957e8ba3ec5d1e
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-01-14 13:12:55 +0000

    add eventsToDays function

--- oncall-scheduler/script.js
+++ oncall-scheduler/script.js
@@ -1,3 +1,53 @@
+function getNumberOfDays(start, end) {
+    const firstDate = new Date(start);
+    const secondDate = new Date(end);
+
+    const oneDayInMs = 1000 * 60 * 60 * 24;
+    const diffInTime = secondDate.getTime() - firstDate.getTime();
+    const diffInDays = Math.round(diffInTime / oneDayInMs);
+
+    return diffInDays;
+}
+
+function weekday(date) {
+    const day = date.getDay()
+    if (day > 0 && day < 6) {
+        return day
+    }
+
+    return false
+}
+
+function eventsToDays(events) {
+    const daysOff = []
+
+    for (var event of events) {
+        const start = new Date(Date.parse(event.start.dateTime))
+        start.setHours(0,0,0,0)
+        const end = new Date(Date.parse(event.end.dateTime))
+        end.setHours(0,0,0,0)
+        
+        const numDaysOff = getNumberOfDays(start, end)
+
+        // if this is a single OOO no need to loop
+        if (numDaysOff === 1) {
+            if (weekday(start)) {
+                daysOff.push(start)
+            }
+        } else {
+            
+            for (start; start < end; start.setDate(start.getDate() + 1)) {
+                if (weekday(start)) {
+                    daysOff.push(new Date(start))
+                }
+            }
+        }
+    }
+
+    return daysOff
+    
+}
+
 function getWeeks(start = new Date(), end = new Date(new Date().getFullYear(), 11, 31)) { 
     const weeks = []
     var week = []
@@ -7,9 +57,8 @@ function getWeeks(start = new Date(), end = new Date(new Date().getFullYear(), 1
 
     // loop through days
     for (var d = start; d < end; d.setDate(d.getDate() + 1)) {
-        // if the day is a week day
-        const day = d.getDay()
-        if (day > 0 && day < 6) {
+        const day = weekday(d)
+        if (day) {
             var date = d
             date.setHours(0,0,0,0)
 
@@ -31,5 +80,6 @@ function getWeeks(start = new Date(), end = new Date(new Date().getFullYear(), 1
 }
 
 module.exports = {
-    getWeeks
+    getWeeks,
+    eventsToDays
 }
