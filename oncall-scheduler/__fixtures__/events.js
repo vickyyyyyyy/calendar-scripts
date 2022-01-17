73d1a287668ed83bf58d48e47f7d8f5d235b530d
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-01-17 12:28:11 +0000

    add fixture for calendar list response

--- oncall-scheduler/__fixtures__/events.js
+++ oncall-scheduler/__fixtures__/events.js
@@ -26,6 +26,165 @@ const OOOEvent = (start = "2022-01-17T00:00:00-05:00", end = "2022-01-18T00:00:0
        dateTime: start },
     creator: { email: Chance().email(), self: true } })
 
+const calendarListResponses = () => {
+  const username = `${Chance().first()}.${Chance().last()}`.toLowerCase()
+  const email = `${username}@grafana.com`
+  const timeZone = 'Europe/London'
+  const updated = Chance().date({string: true})
+  const nextSyncToken = Chance().guid()
+  const differentNextSyncToken = Chance().guid()
+  const etag = Chance().string()
+
+  return [[
+    {
+      updated,
+      summary: email,
+      timeZone,
+      nextSyncToken,
+      items: [],
+      accessRole: "owner",
+      defaultReminders: [
+          {
+              minutes: 10,
+              method: "popup"
+          }
+      ],
+      kind: "calendar#events",
+      etag
+  },
+  {
+      updated,
+      items: [],
+      accessRole: "owner",
+      summary: email,
+      timeZone,
+      etag,
+      kind: "calendar#events",
+      defaultReminders: [
+          {
+              minutes: 10,
+              method: "popup"
+          }
+      ],
+      nextSyncToken: differentNextSyncToken
+  },
+  {
+      updated,
+      etag,
+      items: [],
+      summary: email,
+      accessRole: "owner",
+      timeZone,
+      nextSyncToken,
+      kind: "calendar#events",
+      defaultReminders: [
+          {
+              method: "popup",
+              minutes: 10
+          }
+      ]
+  },
+  {
+      kind: "calendar#events",
+      nextSyncToken,
+      updated,
+      defaultReminders: [
+          {
+              minutes: 10,
+              method: "popup"
+          }
+      ],
+      accessRole: "owner",
+      items: [],
+      etag,
+      summary: email,
+      timeZone
+  },
+  {
+    defaultReminders: [
+        {
+            minutes: 10,
+            method: "popup"
+        }
+    ],
+    items: [
+        {
+            reminders: {
+                useDefault: false
+            },
+            visibility: "public",
+            kind: "calendar#event",
+            iCalUID: Chance().guid(),
+            status: "confirmed",
+            updated,
+            id: Chance().guid(),
+            attendees: [
+                {
+                    self: true,
+                    email,
+                    displayName: email,
+                    responseStatus: "accepted"
+                }
+            ],
+            htmlLink: Chance().url(),
+            sequence: 0,
+            organizer: {
+                email: "unknownorganizer@calendar.google.com",
+                displayName: "Unknown Organizer"
+            },
+            etag: Chance().guid(),
+            start: {
+                timeZone,
+                dateTime: "2022-04-11T00:00:00+01:00"
+            },
+            end: {
+                dateTime: "2022-04-16T00:00:00+01:00",
+                timeZone
+            },
+            privateCopy: true,
+            summary: "Out of office",
+            created: updated,
+            eventType: "outOfOffice",
+            creator: {
+                self: true,
+                email
+            }
+        }
+    ],
+    etag: Chance().guid(),
+    updated,
+    accessRole: "owner",
+    summary: email,
+    kind: "calendar#events",
+    nextSyncToken: Chance().guid(),
+    timeZone
+  },
+  {
+      accessRole: "owner",
+      kind: "calendar#events",
+      nextSyncToken,
+      updated,
+      items: [],
+      timeZone,
+      defaultReminders: [
+          {
+              minutes: 10,
+              method: "popup"
+          }
+      ],
+      etag,
+      summary: email
+  }
+  ], [
+    new Date('2022-04-11'),
+    new Date('2022-04-12'),
+    new Date('2022-04-13'),
+    new Date('2022-04-14'),
+    new Date('2022-04-15'),
+  ]]
+}
+
 module.exports = {
-    OOOEvent
+    OOOEvent,
+    calendarListResponses
 }
