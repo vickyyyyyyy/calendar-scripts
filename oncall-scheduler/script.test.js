8961ca50ad97f5f77853a07e876770ce2175bb52
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-01-17 16:43:58 +0000

    import script in tests

--- oncall-scheduler/script.test.js
+++ oncall-scheduler/script.test.js
@@ -1,4 +1,4 @@
-const { eventsToDays, getWeeks, getOOO, schedule } = require("./script")
+const script = require("./script")
 const weekDates = require("./__fixtures__/weekDates")
 const events = require("./__fixtures__/events")
 const google = require("./__fixtures__/google")
@@ -22,7 +22,7 @@ describe("script", () => {
                 .mockReturnValueOnce(response[4])
                 .mockReturnValueOnce(response[5])
 
-            expect(getOOO(google.users)).toEqual({
+            expect(script.getOOO(google.users)).toEqual({
                 "taylor.swift": expectedDaysOff,
                 "nicki.minaj": expectedDaysOff,
             })
@@ -31,13 +31,13 @@ describe("script", () => {
 
     describe("eventsToDays", () => {
         it("returns single day off for a Monday to Tuesday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent()
             ])).toEqual([new Date('2022-01-17')])
         })
 
         it("returns two days off for a Monday to Wednesday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-17T00:00:00-05:00", "2022-01-19T00:00:00-05:00"),
             ])).toEqual([
                 new Date('2022-01-17'),
@@ -46,7 +46,7 @@ describe("script", () => {
         })
 
         it("returns three days off for a Monday to Thursday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-17T00:00:00-05:00", "2022-01-20T00:00:00-05:00"),
             ])).toEqual([
                 new Date('2022-01-17'),
@@ -56,44 +56,44 @@ describe("script", () => {
         })
 
         it("returns five days off for a Monday to Saturday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-03T00:00:00-05:00", "2022-01-08T00:00:00-05:00"),
             ])).toEqual(...weekDates.weekdaysForOneWeek)
         })
 
         it("returns five days off for a Monday to Sunday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-03T00:00:00-05:00", "2022-01-09T00:00:00-05:00"),
             ])).toEqual(...weekDates.weekdaysForOneWeek)
         })
 
         it("returns five days off for a Monday to Monday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-03T00:00:00-05:00", "2022-01-10T00:00:00-05:00"),
             ])).toEqual(...weekDates.weekdaysForOneWeek)
         })
 
         it("does not return days off for a Saturday to Sunday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-01T00:00:00-05:00", "2022-01-02T00:00:00-05:00"),
             ])).toEqual([])
         })
 
         it("does not return days off for a Saturday to Monday OOO event", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-01T00:00:00-05:00", "2022-01-03T00:00:00-05:00"),
             ])).toEqual([])
         })
 
         it("returns days off for two midweek OOO events", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-03T00:00:00-05:00", "2022-01-05T00:00:00-05:00"),
                 events.OOOEvent("2022-01-05T00:00:00-05:00", "2022-01-08T00:00:00-05:00"),
             ])).toEqual(...weekDates.weekdaysForOneWeek)
         })
 
         it("returns days off for two week-long OOO events", () => {
-            expect(eventsToDays([
+            expect(script.eventsToDays([
                 events.OOOEvent("2022-01-01T00:00:00-05:00", "2022-01-08T00:00:00-05:00"),
                 events.OOOEvent("2022-01-10T00:00:00-05:00", "2022-01-17T00:00:00-05:00"),
             ])).toEqual(weekDates.weekdaysForTwoWeeks.flat())
@@ -112,7 +112,7 @@ describe("script", () => {
             });
     
             it("returns correct dates for year of 2022", () => {
-                expect(getWeeks()).toEqual(weekDates.weekdaysFor2022)
+                expect(script.getWeeks()).toEqual(weekDates.weekdaysFor2022)
             })
         })
 
@@ -121,49 +121,49 @@ describe("script", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-07')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
             })
     
             it("returns correct dates given start day of Monday and end day of Saturday", () => {
                 const startDate = new Date('2022-01-01')
                 const endDate = new Date('2022-01-08')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
             })
         
             it("returns correct dates given start day of Monday and end day of Sunday", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-09')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForOneWeek)
             })
     
             it("returns correct dates given start day of Monday and end day of Wednesday", () => {
                 const startDate = new Date('2022-01-10')
                 const endDate = new Date('2022-01-12')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromStartToMidForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromStartToMidForOneWeek)
             })
 
             it("returns correct dates given start day of Wednesday and end day of Friday", () => {
                 const startDate = new Date('2022-01-12')
                 const endDate = new Date('2022-01-14')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
             })
 
             it("returns correct dates given start day of Wednesday and end day of Saturday", () => {
                 const startDate = new Date('2022-01-12')
                 const endDate = new Date('2022-01-15')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
             })
 
             it("returns correct dates given start day of Wednesday and end day of Sunday", () => {
                 const startDate = new Date('2022-01-12')
                 const endDate = new Date('2022-01-16')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForOneWeek)
             })
         })
 
@@ -172,61 +172,61 @@ describe("script", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-14')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
             })
         
             it("returns correct dates given start date of Monday and end date of Saturday", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-15')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
             })
         
             it("returns correct dates given start date of Monday and end date of Sunday", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-16')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysForTwoWeeks)
             })
 
             it("returns correct dates given start date of Monday and end date of Wednesday", () => {
                 const startDate = new Date('2022-01-03')
                 const endDate = new Date('2022-01-12')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromStartToMidForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromStartToMidForTwoWeeks)
             })
 
             it("returns correct dates given start date of Wednesday and end date of Friday", () => {
                 const startDate = new Date('2022-01-05')
                 const endDate = new Date('2022-01-14')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
             })
 
             it("returns correct dates given start date of Wednesday and end date of Saturday", () => {
                 const startDate = new Date('2022-01-05')
                 const endDate = new Date('2022-01-15')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
             })
 
             it("returns correct dates given start date of Wednesday and end date of Sunday", () => {
                 const startDate = new Date('2022-01-05')
                 const endDate = new Date('2022-01-16')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToEndForTwoWeeks)
             })
 
             it("returns correct dates given start date of Wednesday and end date of Wednesday", () => {
                 const startDate = new Date('2022-01-05')
                 const endDate = new Date('2022-01-12')
         
-                expect(getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToMidForTwoWeeks)
+                expect(script.getWeeks(startDate, endDate)).toEqual(weekDates.weekdaysFromMidToMidForTwoWeeks)
             })
         })
     })
 
-    describe("schedule", () => {
+    describe("scheduler", () => {
         it("returns schedule with no overlapping OOO", () => {
             const expectedDaysOff = events.calendarListResponses()[1]
             const ooo = {
@@ -235,7 +235,7 @@ describe("script", () => {
                 "ariana.grande": expectedDaysOff,
                 "hayley.kiyoko": expectedDaysOff
             }
-            expect(schedule(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
+            expect(script.scheduler(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
                 [
                     "taylor.swift",
                     "nicki.minaj",
@@ -261,7 +261,7 @@ describe("script", () => {
                 "ariana.grande": expectedDaysOff,
                 "hayley.kiyoko": expectedDaysOff
             }
-            expect(schedule(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
+            expect(script.scheduler(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
                 [
                     "taylor.swift",
                     "nicki.minaj",
@@ -291,7 +291,7 @@ describe("script", () => {
                 "ariana.grande": expectedDaysOff,
                 "hayley.kiyoko": expectedDaysOff
             }
-            expect(schedule(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
+            expect(script.scheduler(ooo, weekDates.weekdaysFor2022.slice(0,3))).toEqual([
                 [
                     "nicki.minaj",
                     "ariana.grande",
